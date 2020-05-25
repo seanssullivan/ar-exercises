34c6aa0bba56d9a49250be089e540be8f4fbb8c4
Sean Sullivan <dev@seanmedia.ca>
2020-05-25 23:23:18 +0000

    Exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 45)
+@store1.employees.create(first_name: "Hafiz", last_name: "Suara", hourly_rate: 45)
+@store1.employees.create(first_name: "Glen", last_name: "Chua", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Cassie", last_name: "Rheaume", hourly_rate: 60)
+@store2.employees.create(first_name: "Meri", last_name: "Ghazaryan", hourly_rate: 60)
